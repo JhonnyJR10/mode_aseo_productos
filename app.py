2c63f4bbf22d6e8696a18c1daf6c78bcc274861c
Jhonny <Jhonnyjesusruiz2000@gmail.com>
2023-07-18 18:58:21 -0500

    a

--- app.py
+++ app.py
@@ -1,39 +1,31 @@
 import pickle
 import pandas as pd
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 # Cargar las predicciones desde el archivo
 with open('predicciones.pckl', 'rb') as file:
-    predicciones_cargadas = pickle.load(file)
+    predictions = pickle.load(file)
 
 @app.route("/katana-ml/api/v1.0/forecast/ironsteel", methods=['POST'])
 def predict():
-    # Obtener los datos de entrada
-    data = request.json['data']
     horizon = int(request.json['horizon'])
+    producto = request.json['producto']
     
-    future = predicciones_cargadas.make_future_dataframe(periods=horizon, freq='M')
-    forecast = predicciones_cargadas.predict(future)
+    # Obtener las predicciones correspondientes al producto
+    if producto in predictions:
+        forecast = predictions[producto]
+        data = forecast.tail(horizon)[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    else:
+        data = pd.DataFrame()  # Producto no encontrado
     
-    # Filtrar los datos para los productos específicos
-    data_filtered = forecast[forecast['producto'].isin(data)]
-    
-    # Obtener las últimas 'horizon' filas
-    data_filtered = data_filtered.tail(horizon)
-    
-    # Seleccionar las columnas requeridas
-    data_selected = data_filtered[['producto', 'ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-    
-    # Convertir a formato JSON
-    ret = data_selected.to_json(orient='records', date_format='iso')
+    ret = data.to_json(orient='records', date_format='iso')
     
     return ret
 
-# running REST interface, port=3000 for direct test
+# Ejecutar la aplicación Flask
 if __name__ == "__main__":
     app.run()
