99ba72d4ae8facbadec6453e712718f58376622c
Jhonny <Jhonnyjesusruiz2000@gmail.com>
2023-07-18 16:43:12 -0500

    a

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import pickle
 import pandas as pd
-with open('predicciones.pckl', 'rb') as fin:
-    m2 = pickle.load(fin)
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
 CORS(app)
+
 @app.route("/katana-ml/api/v1.0/forecast/ironsteel", methods=['POST'])
 def predict():
     horizon = int(request.json['horizon'])
@@ -19,6 +18,11 @@ def predict():
     ret = data.to_json(orient='records', date_format='iso')
     
     return ret
+
+# Load the Prophet model from pickle file
+with open('predicciones.pckl', 'rb') as fin:
+    m2 = pickle.load(fin)
+
 # running REST interface, port=3000 for direct test
 if __name__ == "__main__":
     app.run()
